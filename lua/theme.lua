0fabf8e850d5fd01d399216bbd043157468b919e
Jordi Keijzers <jordikeijzers@gmail.com>
2025-10-30 18:35:34 +0100

    Update

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,4 +1,6 @@
 -- vim.cmd.colorscheme "themer_astron"
 -- vim.cmd.colorscheme "themer_sakura"
 -- vim.cmd.colorscheme "themer_onedark"
-vim.cmd.colorscheme = "themer_sonokai_deep"
+-- vim.cmd.colorscheme = "themer_sonokai_deep"
+-- vim.cmd.colorscheme = "themer_jellybeans"
+vim.cmd [[ colorscheme oxocarbon ]]
